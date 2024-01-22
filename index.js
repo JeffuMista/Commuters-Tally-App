7bbfa0e0dfa2de0f32f7f7c85211e7ed67a83337
Jeffu Devops <gnmista@gmail.com>
2024-01-22 09:57:22 +0300

    Total count

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
 let count = 0;
-let  countEl = document.getElementById("count-el");
+let sum = 0
+let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("save-el");
+let totalCount = document.getElementById("total-count")
 let incrementBtn = document.getElementById("increment-btn");
+let saveArray = []
 console.log(saveEl)
 
 function passIncrement(){
@@ -13,6 +16,13 @@ function passIncrement(){
 function save(){
     let countStr = count + " - "
     saveEl.textContent += countStr
+    let currentSave = countStr.replace(/[^0-9]/g, '');
+    currentSave = parseFloat(currentSave)
+    console.log(currentSave)
+    saveArray.push(currentSave)
+    sum =saveArray.reduce((a, b) => a + b, 0);
+    let total = "Total Count: "
+    totalCount.textContent = total + sum
     countEl.textContent = 0
     count = 0
 };
